b0745db51b6977209163a04a3b037468b0c8ba06
Mansur <cybermans@example.com>
2023-01-25 19:55:47 +0300

    upgrade

--- TestProject1/Program.cs
+++ TestProject1/Program.cs
@@ -48,6 +48,7 @@ int r2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a column index for 2nd element: ");
 int c2 = Convert.ToInt32(Console.ReadLine());
 Print2dArray(Change2dArray(newArray, r1, c1, r2, c2));
+Console.WriteLine();
 
 // Дан двумерный массив. Определить сумму элементов на его i-й строке 
 int FindSumOfElemOnRow(int[,] array, int rowNumber){
@@ -74,3 +75,48 @@ int rowNumber = Convert.ToInt32(Console.ReadLine());
 
 int result = FindSumOfElemOnRow(newArr, rowNumber);
 Console.WriteLine($"The sum of elements on {rowNumber} row is {result}");
+Console.WriteLine();
+
+// Дан двумерный массив. Найти количество и сумму положительных элементов массива
+int[,] Create2dArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(-10, 10);
+    return array;
+}
+
+int SumOfPositiveElements(int[,] collection){
+    int sum = 0;
+    for(int i = 0; i < collection.GetLength(0); i++){
+        for(int j = 0; j < collection.GetLength(1); j++)
+            if(collection[i,j] > 0) sum += collection[i,j];
+    }
+    return sum;
+}
+
+int CountOfPositiveElements(int[,] col){
+    int count = 0;
+    for(int i = 0; i < col.GetLength(0); i++){
+        for(int j = 0; j < col.GetLength(1); j++)
+            if(col[i,j] > 0) count++;
+    }
+    return count;
+}
+Console.Write("Input rows count: ");
+int rr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input columns count: ");
+int cc = Convert.ToInt32(Console.ReadLine());
+
+int[,] newCreateArray = Create2dArray(rr, cc);
+Print2dArray(newCreateArray);
+
+// сумма положительных элементов массива
+int sumOfPositiveElem = SumOfPositiveElements(newCreateArray);
+Console.WriteLine($"The sum of positive elements is {sumOfPositiveElem}");
+
+// количество положительных элементов массива
+int countOfPositiveElem = CountOfPositiveElements(newCreateArray);
+Console.WriteLine($"The count of positive elements is {countOfPositiveElem}");
+Console.WriteLine();
